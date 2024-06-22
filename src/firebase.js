715f8214a3cbe42d08a559917f28b4aa734ee399
Areej Sammour <122778464+AreejSammour@users.noreply.github.com>
2024-06-22 18:06:33 +0200

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -3,12 +3,12 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore" ;
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAgRm09fXolFqlBlyQKEocVKXHsf8tmlqo",
-  authDomain: "javascriptquiz-1c87b.firebaseapp.com",
-  projectId: "javascriptquiz-1c87b",
-  storageBucket: "javascriptquiz-1c87b.appspot.com",
-  messagingSenderId: "42064216317",
-  appId: "1:42064216317:web:147d2647d0824834377e94"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: ""
 };
 
 // Initialize Firebase
